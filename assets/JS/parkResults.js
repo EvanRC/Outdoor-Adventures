cca6a5b263c6b77280f891c98727dbcbae75b635
t4k1 <johncurtis702@gmail.com>
2023-11-06 16:32:28 -0800

    add search history using local storage+minor fixes

--- assets/JS/parkResults.js
+++ assets/JS/parkResults.js
@@ -15,92 +15,98 @@ const backToTopBtn = document.getElementById('topBtn')
 
 // Initialize the carousel component when the document loads
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
+  var elems = document.querySelectorAll('.carousel')
   var options = {
     fullWidth: true, // Makes the carousel full width
     indicators: true,
-  };
-  var instances = M.Carousel.init(elems, options);
+  }
+  var instances = M.Carousel.init(elems, options)
 
   // Auto slide change every 4 seconds
   var interval = setInterval(function () {
-    var instance = M.Carousel.getInstance(elems[0]);
-    instance.next(); // Move to the next slide
-  }, 4000);
-});
+    var instance = M.Carousel.getInstance(elems[0])
+    instance.next() // Move to the next slide
+  }, 4000)
+})
 // Initialize all other Materialize components
-M.AutoInit();
+M.AutoInit()
 
 // Function to fetch weather information and forecasts using OpenWeather API
 function fetchWeatherAndForecast(lat, lon, callback) {
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${opWeatherKey}&units=imperial`;
-  const foreCastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${opWeatherKey}&units=imperial`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${opWeatherKey}&units=imperial`
+  const foreCastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${opWeatherKey}&units=imperial`
   // Fetch the current weather
   fetch(weatherUrl)
-    .then(response => response.json())
-    .then(weatherData => {
+    .then((response) => response.json())
+    .then((weatherData) => {
       // If the response contains weather data
       if (weatherData && weatherData.main && weatherData.weather) {
         // Extract necessary data from the weatherData response
-        const temperature = weatherData.main.temp; // Current temperature
-        const description = weatherData.weather[0].description; // Weather condition description
-        const iconCode = weatherData.weather[0].icon; // Weather icon code
-        const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`; // URL for the weather icon
+        const temperature = weatherData.main.temp // Current temperature
+        const description = weatherData.weather[0].description // Weather condition description
+        const iconCode = weatherData.weather[0].icon // Weather icon code
+        const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png` // URL for the weather icon
 
         // Process the weather data and build an HTML string for display
         const formattedWeatherInfo = `
       <strong> Today:</strong> <br>
       Temperature: ${temperature}°F, 
-      Condition: ${description} <img src="${iconUrl}" alt="${description} icon"><br>`; //current weather
+      Condition: ${description} <img src="${iconUrl}" alt="${description} icon"><br>` //current weather
 
-        return Promise.all([fetch(foreCastUrl), formattedWeatherInfo]);   // Returns a Promise that resolves with both the forecast data and the current weather HTML string
-
+        return Promise.all([fetch(foreCastUrl), formattedWeatherInfo]) // Returns a Promise that resolves with both the forecast data and the current weather HTML string
       } else {
-         // If the weather data is not available, throw an error
-        throw new Error("No weather forecast information available currently");
+        // If the weather data is not available, throw an error
+        throw new Error('No weather forecast information available currently')
       }
     })
-    .then(([forecastResponse, formattedWeatherInfo]) => { //process weather data and builds HTML string to display 5 day forecast
-      return forecastResponse.json().then(forecastData => {
-        let forecastHTML = '<br><strong>5-day Forecast:</strong><br>';
+    .then(([forecastResponse, formattedWeatherInfo]) => {
+      //process weather data and builds HTML string to display 5 day forecast
+      return forecastResponse.json().then((forecastData) => {
+        let forecastHTML = '<br><strong>5-day Forecast:</strong><br>'
 
         // Array to map numerical day of the week to string (e.g., 0 -> 'Sunday')
-        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const daysOfWeek = [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+        ]
 
-         // Check if forecastData has a list of forecast items
+        // Check if forecastData has a list of forecast items
         if (forecastData && forecastData.list) {
-           // Loop through the forecast list items (every 8th item represents a new day)
+          // Loop through the forecast list items (every 8th item represents a new day)
           for (let i = 0; i < forecastData.list.length; i += 8) {
-            const dayData = forecastData.list[i];
-            const dayTemp = dayData.main.temp;
-            const dayDescription = dayData.weather[0].description;
-            const dayIconCode = dayData.weather[0].icon;
-            const dayIconUrl = `https://openweathermap.org/img/w/${dayIconCode}.png`;
+            const dayData = forecastData.list[i]
+            const dayTemp = dayData.main.temp
+            const dayDescription = dayData.weather[0].description
+            const dayIconCode = dayData.weather[0].icon
+            const dayIconUrl = `https://openweathermap.org/img/w/${dayIconCode}.png`
 
-            const forecastDate = new Date(dayData.dt * 1000);
+            const forecastDate = new Date(dayData.dt * 1000)
 
-            const dayOfWeek = daysOfWeek[forecastDate.getDay()];
+            const dayOfWeek = daysOfWeek[forecastDate.getDay()]
 
             forecastHTML += `
      <strong>${dayOfWeek}:</strong>&nbsp;&nbsp;
-      Temperature: ${dayTemp}°F, Conditions: ${dayDescription} <img src="${dayIconUrl}" alt="${dayDescription} icon"><br>`; //5day forecast
+      Temperature: ${dayTemp}°F, Conditions: ${dayDescription} <img src="${dayIconUrl}" alt="${dayDescription} icon"><br>` //5day forecast
           }
         }
 
-        const combinedWeatherInfo = formattedWeatherInfo + forecastHTML;
-        
-         // Execute the callback function passing the concatenated current weather and forecast HTML
-        callback(null, combinedWeatherInfo);
-      });
+        const combinedWeatherInfo = formattedWeatherInfo + forecastHTML
+
+        // Execute the callback function passing the concatenated current weather and forecast HTML
+        callback(null, combinedWeatherInfo)
+      })
     })
-    .catch(error => {
-       // If there is an error fetching the weather or forecast, execute the callback with the error
-       callback(error, null);
-      callback(error);
-    });
-
+    .catch((error) => {
+      // If there is an error fetching the weather or forecast, execute the callback with the error
+      callback(error, null)
+      callback(error)
+    })
 }
-
 
 // Function to create park cards with fetched data
 function buildCards(filteredParkList) {
@@ -165,26 +171,24 @@ function buildCards(filteredParkList) {
             </div>
           </div>
         `
-        
 
-      const lat = park.latitude; // Extract the latitude and longitude from the park object.
-      const lon = park.longitude;
+      const lat = park.latitude // Extract the latitude and longitude from the park object.
+      const lon = park.longitude
 
       // Fetch weather and forecast data using the extracted latitude and longitude.
       fetchWeatherAndForecast(lat, lon, (error, weatherInfo) => {
-          // Check if there was an error during the fetch
+        // Check if there was an error during the fetch
         if (error) {
           // Log the error to the console if a fetch error occurs.
-          console.error('Failed to fetch the weather:', error);
+          console.error('Failed to fetch the weather:', error)
         } else {
           // Find the DOM element for displaying weather information within the park card.
-          const weatherDiv = parkCard.querySelector('.weather-info-placeholder');
+          const weatherDiv = parkCard.querySelector('.weather-info-placeholder')
           if (weatherDiv) {
-            weatherDiv.innerHTML = weatherInfo;
+            weatherDiv.innerHTML = weatherInfo
           }
         }
       })
-
 
       const cardTitle = parkCard.querySelector('.card-title.activator') // Select the element that serves as the trigger to reveal additional park information.
       const cardReveal = parkCard.querySelector('.card-reveal') // Select the element that contains the additional park information.
@@ -194,7 +198,7 @@ function buildCards(filteredParkList) {
       })
 
       const tabLinks = parkCard.querySelectorAll('.card-tabs a')
-      
+
       tabLinks.forEach((tabLink) => {
         tabLink.addEventListener('click', (event) => {
           const tabId = tabLink.getAttribute('href').substring(1)
@@ -225,7 +229,7 @@ function fetchParkNames() {
     },
   })
     .then((response) => {
-       // Throw an error if the response status is not ok to catch it later.
+      // Throw an error if the response status is not ok to catch it later.
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
@@ -233,15 +237,14 @@ function fetchParkNames() {
       return response.json()
     })
     .then((data) => {
-
-          // Log the received data to the console for debugging.
+      // Log the received data to the console for debugging.
       console.log(data)
-       // Call the function to process parks within the specified radius using the received data.
+      // Call the function to process parks within the specified radius using the received data.
       searchParksWithinRadius(data.data)
     })
 
     .catch((error) => {
-        // Log any errors that occur during the fetch operation.
+      // Log any errors that occur during the fetch operation.
       console.error('There was a problem with the fetch operation:', error)
     })
 }
@@ -251,13 +254,14 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 // Function to construct HTML for entrance fees information
 function buildFeeInfoHTML(entranceFees) {
-    // If there are no entrance fees provided, return a message indicating this.
+  // If there are no entrance fees provided, return a message indicating this.
   if (!entranceFees || entranceFees.length === 0) {
     return '<div class="no-fees-info">No entrance fee information available for this park.</div>'
   }
 
   let feeInfoHTML = '' // Initialize an empty string to hold the HTML content for entrance fees.
-  entranceFees.forEach((entranceFees) => { // Loop through each entrance fee item and append its information to the HTML string.
+  entranceFees.forEach((entranceFees) => {
+    // Loop through each entrance fee item and append its information to the HTML string.
     feeInfoHTML += `
       <div>
         <strong>${entranceFees.title}:</strong> $${entranceFees.cost}<br>
@@ -286,7 +290,8 @@ function geocodeLocation(location) {
     .then(function (response) {
       return response.json() // Parse the response as JSON.
     })
-    .then(function (data) {  // Extract the coordinates from the geocode data.
+    .then(function (data) {
+      // Extract the coordinates from the geocode data.
       var coordinates = data.results[0].geometry.location
       return coordinates
     })
@@ -299,44 +304,48 @@ function geocodeLocation(location) {
 
 // Function to calculate distance between two points on Earth
 function calculateDistance(lat1, lon1, lat2, lon2) {
-  var R = 3958.8  // Radius of the Earth in miles.
-   // Convert the differences in latitude and longitude from degrees to radians.
+  var R = 3958.8 // Radius of the Earth in miles.
+  // Convert the differences in latitude and longitude from degrees to radians.
   var dLat = (lat2 - lat1) * (Math.PI / 180)
   var dLon = (lon2 - lon1) * (Math.PI / 180)
   // Apply the Haversine formula to calculate the distance.
   var a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(lat1 * (Math.PI / 180)) *
-    Math.cos(lat2 * (Math.PI / 180)) *
-    Math.sin(dLon / 2) *
-    Math.sin(dLon / 2)
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2)
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) // Calculate the distance and return it.
   var distance = R * c
   return distance
 }
 // Function to filter parks within a certain radius from user's location
 function searchParksWithinRadius(allParks) {
-  var userLocationInput = document.getElementById('find_parks').value  // Retrieve the user's location input from the input field with the ID 'find_parks'.
-  var selectedRadius = getSelectedRadius() 
+  var userLocationInput = document.getElementById('find_parks').value // Retrieve the user's location input from the input field with the ID 'find_parks'.
+  var selectedRadius = getSelectedRadius()
 
-  if (userLocationInput !== '' && userLocationInput !== null) { // Check if the user has entered a location. If not, display all parks.
-    if (!selectedRadius) { // If no radius is selected, display all parks without filtering by radius.
+  if (userLocationInput !== '' && userLocationInput !== null) {
+    // Check if the user has entered a location. If not, display all parks.
+    if (!selectedRadius) {
+      // If no radius is selected, display all parks without filtering by radius.
       buildCards(allParks)
       return
     }
     geocodeLocation(userLocationInput) // Call the geocodeLocation function to convert the user's location input into geographic coordinates.
       .then(function (userCoordinates) {
         if (!userCoordinates) {
-          alert('Location not found. Please enter a valid city or zip code.')  // If geocoding the location fails, notify the user.
+          alert('Location not found. Please enter a valid city or zip code.') // If geocoding the location fails, notify the user.
         } else {
-          searchPerformed = true     
-          var filteredParks = allParks.filter(function (park) {     // Filter the list of all parks to find those within the selected radius of the user's location.
+          searchPerformed = true
+          var filteredParks = allParks.filter(function (park) {
+            // Filter the list of all parks to find those within the selected radius of the user's location.
             var parkCoordinates =
               park.latitude && park.longitude
                 ? { lat: park.latitude, lng: park.longitude }
                 : null
 
-            if (  // If the park has coordinates and is within the selected radius of the user's location, include it in the filtered list.
+            if (
+              // If the park has coordinates and is within the selected radius of the user's location, include it in the filtered list.
               parkCoordinates &&
               calculateDistance(
                 userCoordinates.lat,
@@ -345,15 +354,15 @@ function searchParksWithinRadius(allParks) {
                 parkCoordinates.lng
               ) <= selectedRadius
             ) {
-              return true    
+              return true
             }
-            return false    // Otherwise, do not include the park in the filtered list.
+            return false // Otherwise, do not include the park in the filtered list.
           })
-          buildCards(filteredParks)  // Use the filtered list of parks to build the UI cards that display park information.
+          buildCards(filteredParks) // Use the filtered list of parks to build the UI cards that display park information.
         }
       })
       .catch(function (error) {
-        console.error('Error searching parks within radius:', error)  // Catch any errors during the geocoding process or filtering and log them to the console.
+        console.error('Error searching parks within radius:', error) // Catch any errors during the geocoding process or filtering and log them to the console.
       })
   } else {
     buildCards(allParks)
@@ -374,4 +383,45 @@ searchBtn.addEventListener('click', fetchParkNames)
 // Event listener to refresh the page when the refresh button is clicked
 refreshBtn.addEventListener('click', function () {
   location.reload()
+})
+
+//Function to check for and pull saved search history
+document.addEventListener('DOMContentLoaded', function () {
+  const storedSearchHistory = localStorage.getItem('searchHistory')
+  if (storedSearchHistory) {
+    searchHistory = JSON.parse(storedSearchHistory)
+    displaySearchHistory()
+  }
+})
+
+// Function to add a city to the search history
+function addToSearchHistory(city) {
+  searchHistory.unshift(city)
+  if (searchHistory.length > 3) {
+    searchHistory.pop()
+  }
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+  displaySearchHistory()
+}
+
+// Function to display the search history on the web page
+function displaySearchHistory() {
+  const historyContainer = document.getElementById('search-history')
+  historyContainer.innerHTML = '<p>Search History:</p>'
+
+  for (let i = 0; i < searchHistory.length; i++) {
+    const city = searchHistory[i]
+    const historyItem = document.createElement('p')
+    historyItem.textContent = `${i + 1}. ${city}`
+    historyContainer.appendChild(historyItem)
+  }
+}
+
+// Add an event listener to the search button to update the search history
+searchBtn.addEventListener('click', function () {
+  const userLocationInput = document.getElementById('find_parks').value
+  if (userLocationInput.trim() !== '') {
+    addToSearchHistory(userLocationInput)
+  }
+  fetchParkNames()
 })
